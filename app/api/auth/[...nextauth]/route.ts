dfc1652acb1a46b3cc9e7d3e3b8a9bcd8f064376
TanishqJangir <tanishqjangir001@gmail.com>
2026-02-01 17:52:46 +0530

    added the signin and logout functionality on home page

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,54 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
+
+
+const handler = NextAuth({
+
+providers: [
+  CredentialsProvider({
+    // The name to display on the sign in form (e.g. "Sign in with...")
+    name: "Login with email",
+    credentials: {
+      username: { label: "Username", type: "text", placeholder: "jsmith" },
+      password: { label: "Password", type: "password" }
+    },
+    async authorize(credentials, req) {
+      const username = credentials?.username;
+      const password = credentials?.password;
+
+      console.log(username)
+      console.log(password)
+
+      const user = {
+        name : "Tanishq",
+        id : "1",
+        username : "harkirat@gmail.com"
+      }
+
+      if(user){
+        return user;
+      }else{
+        return null;
+      }
+
+
+
+      }
+    }),
+
+    GoogleProvider({
+    clientId: "123123",
+    clientSecret: "123123"
+  }),
+
+  GitHubProvider({
+    clientId: "process.env.GITHUB_I",
+    clientSecret: "process.env.GITHUB_SECRET"
+  })
+
+]
+})
+
+export {handler as GET, handler as POST}
